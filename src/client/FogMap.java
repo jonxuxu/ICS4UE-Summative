a38d40ca9691d4839bb7f7b74d3899da20b24236
Jonathan <jonathanxu25@gmail.com>
2019-05-28 12:22:09 -0400

    fog sizing

--- src/client/FogMap.java
+++ src/client/FogMap.java
@@ -4,12 +4,12 @@ public class FogMap {
   int[][] fog;
 
   FogMap(int y, int x){
-    fog = new int[10000][10000];
+    fog = new int[y][x];
   }
 
   public void age(){
-    for(int i = 0; i < 10000; i ++){
-      for(int j = 0; j < 10000; j++){
+    for(int i = 0; i < fog.length; i ++){
+      for(int j = 0; j < fog[i].length; j++){
         if(fog[i][j] == 2){
           fog[i][j] = 1;
         }
@@ -18,7 +18,7 @@ public class FogMap {
   }
 
   public void scout(int y, int x){
-    updateFog(y, x, 50);
+    updateFog(y, x, 5);
   }
   private void updateFog(int y, int x, int distanceLeft){
     // TODO: Implement obstructions
@@ -26,7 +26,7 @@ public class FogMap {
     if(distanceLeft > 0){
       for(int i = -1; i <= 1; i++){
         for(int j = -1; j <= 1; j++){
-          if(y+ i >= 0 && y + i< 10000 && x + j >= 0 && x + j < 10000){
+          if(y+ i >= 0 && y + i< fog.length && x + j >= 0 && x + j < fog.length){
             updateFog(y + i, j + x, distanceLeft -1);
           }
         }
