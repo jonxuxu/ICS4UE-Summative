f19cba986901848331097f25211e06adbe7f3c7f
penguinofwinter <winterbirdwinterbird@gmail.com>
2019-06-10 19:21:11 -0400

    name fixes

--- src/client/Ghost.java
+++ src/client/Ghost.java
@@ -7,10 +7,10 @@ import java.io.File;
 import java.io.IOException;
 
 /**
- * SafeMarksman.java
- * This is
+ * Ghost.java
+ * This is the character class for Ghost
  *
- * @author Will Jeong
+ * @author Will Jeong + company
  * @version 1.0
  * @since 2019-05-19
  */
