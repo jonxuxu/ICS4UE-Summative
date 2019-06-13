a5f013fa24a4f97b9415cfaf20bb691715df8b47
Code-Titan <artemis.sotnikov12@gmail.com>
2019-06-13 10:21:44 -0400

    Batch of commenting

--- src/server/Dead.java
+++ src/server/Dead.java
@@ -1,8 +1,23 @@
 package server;
+
+/**
+ * Dead.java
+ *
+ * The class containing the information an special state for a scenario of a player's death, extending Stun
+ *
+ * @author Will Jeong, Jonathan Xu, Kamron Zaidi, Artem Sotnikov, Kolby Chong, Bill Liu
+ * @since 2019-05-27
+ * @version 1.0
+ */
 class Dead extends Stun{
   private static int DURATION = 300;
   private static int ID = 1;
+
+  /**
+   * Basic constructor, sending up all local data to the Stun superclass
+   */
   Dead(){
     super(DURATION,ID);
   }
+
 }
