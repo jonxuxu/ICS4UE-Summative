81599961ef8f2182fa3988dd44d0f21a5862421d
7Speed <kirbyrules3.14@gmail.com>
2019-06-13 12:50:19 -0400

    REREEEERERE Comments

--- src/server/JuggernautEStun.java
+++ src/server/JuggernautEStun.java
@@ -1,7 +1,19 @@
 package server;
+/**
+ * JuggernautEStun.java
+ *
+ * This is the class representing the status storing the JuggernautEStun info
+ *
+ * @author Will Jeong, Jonathan Xu, Kamron Zaidi, Artem Sotnikov, Kolby Chong, Bill Liu
+ * @version 1.0
+ * @since 2019-05-19
+ */
 class JuggernautEStun extends Stun{
   private static int DURATION = 100;
   private static int ID = 12;
+  /**
+   * Constructor to create a new JuggernautEStun
+   */
   JuggernautEStun(){
     super(DURATION,ID);
   }
--- src/server/JuggernautQStun.java
+++ src/server/JuggernautQStun.java
@@ -1,7 +1,19 @@
 package server;
+/**
+ * JuggernautQStun.java
+ *
+ * This is the class representing the status storing the JuggernautQStun info
+ *
+ * @author Will Jeong, Jonathan Xu, Kamron Zaidi, Artem Sotnikov, Kolby Chong, Bill Liu
+ * @version 1.0
+ * @since 2019-05-19
+ */
 class JuggernautQStun extends Stun{
   private static int DURATION = 100;
   private static int ID = 12;
+   /**
+   * Constructor to create a new JuggernautQStun
+   */
   JuggernautQStun(){
     super(DURATION,ID);
   }
